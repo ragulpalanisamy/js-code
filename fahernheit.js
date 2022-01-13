78f512ff2aa890dcffce20a64e09ead4201b882a
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2022-01-13 20:17:54 +0530

    Add files via upload

---
+++ fahernheit.js
@@ -0,0 +1,11 @@
+//kelvin
+let kelvin = 245;
+console.log(kelvin);
+//celsius
+let celsius = 234;
+celsius -= kelvin; 
+console.log(celsius);
+//fahernheit.
+let fahrenheit = celsius * (9/5) + 32;
+var temp =Math.floor(fahrenheit);
+console.log(`The Temperature is ${temp} degrees fahrenheit`);
