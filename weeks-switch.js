973a458454300dfde2aae0a6d3c96166e80350f1
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2022-01-14 15:25:12 +0530

    Add files via upload

---
+++ weeks-switch.js
@@ -0,0 +1,26 @@
+var a = 5;
+switch (a){
+    case 1:
+        console.log('monday');
+        break;
+    case 2:
+        console.log('tuesday');
+        break;
+    case 3:
+        console.log('wednesday');
+        break;
+    case 4:
+        console.log('thursday');
+        break;
+    case 5:
+        console.log('friday');
+        break;
+    case 6:
+        console.log('saturday');
+        break;
+    case 7:
+        console.log('sunday');
+    default:
+        console('Only seven days only availabe for a week');
+        break;
+}
