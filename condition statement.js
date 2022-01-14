de2cf6f06ea8a343eec0bf29a329892c3a773b3b
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2022-01-14 13:02:48 +0530

    Add files via upload

---
+++ condition statement.js
@@ -0,0 +1,21 @@
+var a = 'tea';
+var b = 'coffee';
+    if (a === 'tea' && b === 'coffee') {
+        console.log('both tea & Coffee');
+    }
+    else {
+        console.log('none of these');
+    }
+
+    var a = 5;
+    var b = 6;
+    var c = 3;
+    if ((a > b) && (a > c)){
+        console.log('A is Greater');
+    } 
+    else if (b > c){
+        console.log('B is greater');
+    }
+    else{
+        return 'C is greater';
+    }
