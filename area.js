1c18c5fd50b6b424e1a05894f898eac3aa7e9855
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2022-01-15 20:05:47 +0530

    Add files via upload

---
+++ area.js
@@ -0,0 +1,12 @@
+function area(length,width){
+    console.log('Area = ' , length * width);
+}
+console.log(area(12,10));
+  
+
+console.log('Another method');
+
+var length = 12;
+var width = 10;
+var area = length * width;
+console.log(area);
