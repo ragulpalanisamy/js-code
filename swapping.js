9587bd09ee39c8abeb059befb40f9c597da3ff7a
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2022-01-21 20:32:35 +0530

    Add files via upload

---
+++ swapping.js
@@ -0,0 +1,15 @@
+let a = 56;
+let b = 64;
+//create a temporary variable
+let temp;
+console.log(`The value of a before swapping: ${a}`);
+console.log(`The value of b before swapping: ${b}`);
+
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
